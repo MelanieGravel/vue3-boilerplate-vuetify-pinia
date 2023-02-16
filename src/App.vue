9ab3adaf3df9c2dd579c682188df9fbcce752f93
Mélanie Gravel <melanie.gravel@outlook.com>
2023-02-16 18:30:24 -0500

    NavigationDrawer Behaviour

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,47 @@
 <script lang="ts">
-import { defineComponent, onMounted, ref } from 'vue';
+import { defineComponent, onMounted, Ref, ref } from 'vue';
+import { useDisplay } from 'vuetify';
 import FakeDataView from './components/FakeDataView.vue';
-
 
 export default defineComponent({
   name: 'App',
   components: {
     FakeDataView,
-  }
+  },
+  setup() {
+    const menuOpen: Ref<boolean> = ref(true);
+    const { mdAndUp } = useDisplay()
+
+    return {
+      menuOpen,
+      mdAndUp,
+    }
+  },
 });
 </script>
 
 <template>
   <v-layout>
-    <v-app-bar title="Quizzez"></v-app-bar>
-    <v-navigation-drawer>
-      <v-list>
-        <v-list-item title="Home" to="/"></v-list-item>
-        <v-list-item title="Fake" to="/fake"></v-list-item>
+    <v-app-bar>
+      <v-app-bar-nav-icon class="d-sm-none" icon="fad fa-bars" @click="menuOpen = !menuOpen"></v-app-bar-nav-icon>
+      <v-app-bar-title class="text-h5"><v-icon>fab fa-vuejs</v-icon> Vue Boilerplate</v-app-bar-title>
+    </v-app-bar>
+    <v-navigation-drawer
+      v-model="menuOpen"
+      :expand-on-hover="mdAndUp"
+      :rail="mdAndUp"
+    >
+      <v-list density="compact" nav>
+        <v-list-item
+          title="Home"
+          to="/"
+          prepend-icon="fass fa-books"
+        ></v-list-item>
+        <v-list-item
+          title="Fake"
+          to="/fake"
+          prepend-icon="fass fa-robot-astromech"
+        ></v-list-item>
       </v-list>
     </v-navigation-drawer>
     <v-main>
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -9,9 +9,9 @@ import * as directives from 'vuetify/directives';
 import { aliases, fa } from 'vuetify/iconsets/fa-svg';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
-// import { fas } from '@fortawesome/pro-solid-svg-icons';
 import { fass } from '@fortawesome/sharp-solid-svg-icons';
 import { fad } from '@fortawesome/pro-duotone-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
 
 // import '../styles/settings.scss';
 // import 'vuetify/lib/styles/main.sass';
@@ -20,6 +20,7 @@ const vuetifyConfig = (app: App) => {
   app.component('font-awesome-icon', FontAwesomeIcon); // Register component globally
   library.add(fad);
   library.add(fass);
+  library.add(fab);
 
   return createVuetify({
     components,
