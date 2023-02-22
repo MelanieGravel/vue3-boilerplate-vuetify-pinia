e619fca0e42138cfefa56179d875d88290bb8307
Mélanie Gravel <melanie.gravel@outlook.com>
2023-02-21 19:50:09 -0500

    Mistake Management

--- src/components/SingleQuizLanding.vue
+++ src/components/SingleQuizLanding.vue
@@ -2,7 +2,7 @@
 import { computed, defineComponent, Ref, ref } from 'vue';
 import HeroImage from './HeroImage.vue';
 import { shuffle } from '../utils';
-import { QuizzItem } from '../models/quizz.model';
+import { QuizzItem, QuizzMistake } from '../models/quizz.model';
 import { quizzes } from '../data/quizzes';
 import { useRoute, useRouter } from 'vue-router';
 
@@ -12,11 +12,13 @@ export default defineComponent({
     HeroImage,
   },
   setup() {
-    let goodAnswers: Ref<number> = ref<number>(0);
-    let currentQuestionIndex: Ref<number> = ref<number>(0);
     const route = useRoute();
     const id = route.params.id;
+
+    let goodAnswers: Ref<number> = ref<number>(0);
+    let currentQuestionIndex: Ref<number> = ref<number>(0);
     let quizData: QuizzItem[] = quizzes[id as string];
+    let quizMistakes: Ref<QuizzMistake[]> = ref([]);
 
     const currentQuestion = computed<QuizzItem>(() => {
       return quizData[currentQuestionIndex.value];
@@ -27,12 +29,21 @@ export default defineComponent({
     });
 
     const score = computed<number>(() => {
-      return Math.round(goodAnswers.value / (quizData.length)*100);
+      return Math.round(goodAnswers.value / (quizData.length) * 100);
     });
 
     function answerQuestion(index: number): void {
       if (index === currentQuestion.value.goodAnswerIndex) {
         goodAnswers.value++;
+      } else {
+        quizMistakes.value.push(
+          {
+            question: currentQuestion.value.question,
+            goodAnswer: currentQuestion.value.answers[currentQuestion.value.goodAnswerIndex],
+            wrongAnswer: currentQuestion.value.answers[index],
+            explanation: currentQuestion.value.explanation || '',
+          },
+        );
       }
       currentQuestionIndex.value++;
     }
@@ -41,6 +52,7 @@ export default defineComponent({
       quizData = shuffle(quizData);
       goodAnswers.value = 0;
       currentQuestionIndex.value = 0;
+      quizMistakes.value = [];
     }
 
     quizData = shuffle(quizData);
@@ -49,10 +61,11 @@ export default defineComponent({
       quizData,
       currentQuestionIndex,
       currentQuestion,
-      answerQuestion,
       goodAnswers,
       canDisplayResult,
       score,
+      quizMistakes,
+      answerQuestion,
       resetTest,
     };
   },
@@ -64,7 +77,7 @@ export default defineComponent({
   <v-container fluid class="justify-center align-center">
     <hero-image
       title="Single Quiz"
-      height="120"
+      :height="120"
     >
     </hero-image>
     <v-card class="ma-auto mt-10 text-center" max-width="800">
@@ -73,15 +86,36 @@ export default defineComponent({
           Quizz over
         </v-card-title>
         <v-card-text>
-          <span class="text-h1">{{score}}%</span>
+          <p class="text-h1">{{ score }}%</p>
+
+          <v-card class="text-left">
+            <template v-for="(mistake) in quizMistakes">
+              <v-card-text class="text-subtitle-1 text-white">{{ mistake.question }}</v-card-text>
+              <v-card-subtitle class="text-error">
+                <v-icon class="mr-4">fad fa-circle-xmark</v-icon>
+                {{ mistake.wrongAnswer }}
+              </v-card-subtitle>
+              <v-card-subtitle class="text-success text-primary-lighten-1">
+                <v-icon class="mr-4">fad fa-circle-check</v-icon>
+                {{ mistake.goodAnswer }}
+              </v-card-subtitle>
+              <v-card-text class="text-grey">{{ mistake.explanation }}</v-card-text>
+            </template>
+          </v-card>
         </v-card-text>
         <v-card-actions class="d-flex justify-space-around">
-          <v-btn color="primary" to="/"><v-icon class="mr-2">fad fa-arrow-left</v-icon>More quizzez</v-btn>
-          <v-btn color="secondary" @click="resetTest"><v-icon class="mr-2">fad fa-rotate-left</v-icon>Try Again</v-btn>
+          <v-btn color="primary" to="/">
+            <v-icon class="mr-2">fad fa-arrow-left</v-icon>
+            More quizzez
+          </v-btn>
+          <v-btn color="secondary" @click="resetTest">
+            <v-icon class="mr-2">fad fa-rotate-left</v-icon>
+            Try Again
+          </v-btn>
         </v-card-actions>
       </template>
 
-      <template v-else >
+      <template v-else>
         <v-card-title>
           Question {{ currentQuestionIndex + 1 }}
         </v-card-title>
--- src/models/quizz.model.ts
+++ src/models/quizz.model.ts
@@ -1,8 +1,13 @@
-
-
 export interface QuizzItem {
   question: string,
   answers: string[],
   goodAnswerIndex: number,
   explanation?: string,
+}
+
+export interface QuizzMistake {
+  question: string,
+  goodAnswer: string,
+  wrongAnswer: string,
+  explanation: string,
 }
